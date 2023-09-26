9cdcad848841961a7ab7095f89dcabc128fe5116
Hien Ho <hvthhien@gmail.com>
2023-09-26 08:55:21 +0700

    Support bigmap update fetching (#9)
    * support fetch bigmap updates by level

--- bigmap.go
+++ bigmap.go
@@ -6,7 +6,29 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 )
+
+type BigmapUpdate struct {
+	ID        uint64    `json:"id"`
+	Level     uint64    `json:"level"`
+	Timestamp time.Time `json:"timestamp"`
+	Bigmap    uint64    `json:"bigmap"`
+	Contract  Account   `json:"contract"`
+	Path      string    `json:"path"`
+	Action    string    `json:"action"`
+	Content   Content   `json:"content"`
+}
+
+type Content struct {
+	Hash  string      `json:"hash"`
+	Key   string      `json:"key"`
+	Value BigmapValue `json:"value"`
+}
+
+type BigmapValue struct {
+	TokenID string `json:"token_id"`
+}
 
 // GetBigMapValueByPointer returns the value of a key in a bigmap.
 func (c *TZKT) GetBigMapValueByPointer(pointer int, key string) ([]byte, error) {
@@ -115,4 +137,39 @@ func (c *TZKT) GetBigMapPointerForContractTokenMetadata(contract string) (int, e
 	}
 
 	return pointers[0], nil
+}
+
+// GetBigmapUpdatesByLevel returns the bigmap updates of given tags
+func (c *TZKT) GetTokenMetadataBigmapUpdatesByLevel(level string, offset, limit int) ([]BigmapUpdate, error) {
+	if limit == 0 {
+		limit = 100
+	}
+
+	v := url.Values{
+		"level.ge": []string{level},
+		"sort":     []string{"level"},
+		"offset":   []string{fmt.Sprint(offset)},
+		"limit":    []string{fmt.Sprint(limit)},
+		"tags.any": []string{"token_metadata"},
+	}
+
+	u := url.URL{
+		Scheme:   "https",
+		Host:     c.endpoint,
+		Path:     "/v1/bigmaps/updates",
+		RawQuery: v.Encode(),
+	}
+
+	var bigmaps []BigmapUpdate
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := c.request(req, &bigmaps); err != nil {
+		return nil, err
+	}
+
+	return bigmaps, nil
 }
